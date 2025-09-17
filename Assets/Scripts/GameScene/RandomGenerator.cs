c7afa697b6a213fbb4533335403186627427e363
Raphael-Aljona <raphael.aljona@gmail.com>
2025-09-17 17:30:44 -0300

    Menu com video feito

--- Assets/Scripts/GameScene/RandomGenerator.cs
+++ Assets/Scripts/GameScene/RandomGenerator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class RandomGenerator : MonoBehaviour
 {
@@ -13,16 +15,10 @@ public class RandomGenerator : MonoBehaviour
     //Moldura
     public int WallOffset = 10; // altera geração/câmera: distância da moldura até a área jogável (para a parade esquerda: -offset; para a direita: width +offset)
 
-
     public GameObject[] obstaclePrefabs;  // Prefabs dos obstáculos (usem TAG "Ground")
-    //public GameObject portalPrefab;       // Prefab do portal (Collider2D isTrigger + PortalManager)
-    public GameObject playerPrefab;       // Prefab do player (TAG "Player" + Rigidbody2D + Collider2D)
-
-
+    public GameObject playerPrefab  ;       // Prefab do player (TAG "Player" + Rigidbody2D + Collider2D)
 
     public int obstacleLayerY = 2; // altera geração: “passo” entre linhas de obstáculos (1 = todas as linhas; 2 = linhas alternadas)
-    //public int minObstacleGapX = 2; // altera geração: espaçamento mínimo após spawnar um obstáculo (garante passagens)
-    //public int maxObstacleGapX = 4;  // altera geração: espaçamento máximo após spawnar um obstáculo
 
     public int minNoSpawnX = 1; // altera geração: avanço (gap) mínimo quando NÃO spawna 
     public int maxNoSpawnX = 2; // altera geração: avanço (gap) máximo quando NÃO spawna
@@ -32,49 +28,88 @@ public class RandomGenerator : MonoBehaviour
     public int offset => WallOffset; // Somente leitura — Distância (em unidades) entre a borda da área jogável e as PAREDES VISUAIS. Aumentar o offset/WallOffset muda apenas o visual;
                                      // não muda a área jogável.
 
+    public Camera camera;
+
+    public float spawnLinesTop = 5f;
+    float currentY = 0f;
+
+    private Queue<GameObject> activeLines = new Queue<GameObject> ();
+
+    float streetCount = 0;
+    float grassCount = 0;
 
     void Start()
     {
-        // Se já gerou uma vez nesta cena, sai (previne duplicar)
-        if (hasGeneratedThisScene) return;
+        
 
-        //Definindo como true, ou seja,instância já foi gerada
-        hasGeneratedThisScene = true;
-
-        //Função para gerar chão, paredes, obstáculos, portal e player proceduralmente
-        GenerateRoom();
-
-    }
-
-
-
-    void GenerateRoom()
-    {
-        if (obstacleLines == null || obstacleLines.Length == 0) return;
-
-        // ponto inicial em Y
-        float currentY = 0f; // ou qualquer valor inicial desejado
-        float xPosition = 0f; // posição fixa em X para todas as linhas
-
-        // Defina quantas linhas você quer gerar
-        int numLines = playableHeight; // ou outra lógica
-
-        for (int i = 0; i < numLines; i++)
+        for (int i = 0; i < 10; i++)
         {
-            GameObject linePrefab = obstacleLines[Random.Range(0, obstacleLines.Length)];
+            SpawnNextLine();
 
-            GameObject newLine = Instantiate(linePrefab, new Vector2(xPosition, currentY), Quaternion.identity, transform);
-
-            float lineHeight = newLine.GetComponent<BoxCollider2D>().size.y * newLine.transform.localScale.y;
-            Debug.Log($"Linha {i} tem altura {lineHeight}");
-
-            currentY += lineHeight;
         }
 
-        // por último, spawn do player
-        Vector2 playerPos = new Vector2(xPosition, 1f);
-        Instantiate(playerPrefab, playerPos, Quaternion.identity, transform);
+    }
+
+    private void Update()
+    {
+        float cameraTop = camera.transform.position.y + camera.orthographicSize;
+
+        while (cameraTop + spawnLinesTop > currentY)
+        {
+            SpawnNextLine();
+        }
+
+        float cameraBottom = camera.transform.position.y - camera.orthographicSize;
+        while (activeLines.Count > 0 && activeLines.Peek().transform.position.y + 10f < cameraBottom)
+        {
+            Destroy (activeLines.Dequeue());
+        }
     }
 
 
+    void SpawnNextLine()
+    {
+        GameObject prefab = SelectPrefab();
+        GameObject line = Instantiate(prefab, new Vector2(0, currentY), Quaternion.identity, transform);
+
+        float h = line.GetComponent<BoxCollider2D>().size.y * line.transform.localScale.y;
+        currentY += h;
+
+        activeLines.Enqueue(line);
+
+        if (line.CompareTag("Street"))
+        {
+            streetCount+= 1;
+            grassCount = 0;
+            Debug.Log($"{streetCount} rua seguida");
+        }
+        else { 
+            grassCount+= 1;
+            streetCount = 0;
+            Debug.Log("contagem resetada"); 
+        }
+    }
+
+    GameObject SelectPrefab()
+    {
+        if(streetCount >= 3)
+        {
+            var isNotStreet = obstacleLines.Where(e => !e.CompareTag("Street")).ToArray();
+
+            if(isNotStreet.Length > 0)
+            {
+                return isNotStreet[Random.Range(0, isNotStreet.Length)];
+            }
+        }
+        if(grassCount >= 3)
+        {
+            var isNotGrass = obstacleLines.Where(e => e.CompareTag("Street")).ToArray();
+            if (isNotGrass.Length > 0)
+            {
+                return isNotGrass[Random.Range(0, isNotGrass.Length)];
+            }
+        }
+
+        return obstacleLines[Random.Range(0, obstacleLines.Length - 1)];
+    }
 }
--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,19 +1,8 @@
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
-{
-    void Start()
-    {
-            
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+{ 
 
     public void PlayGame()
     {
